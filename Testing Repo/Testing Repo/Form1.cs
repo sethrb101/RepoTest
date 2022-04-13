2e807b783b763ec693884252315daa49acefd4a2
Seth Blackburn <sethrb101@gwu.edu>
2022-04-12 21:03:32 -0400

    Changed a button

--- Testing Repo/Testing Repo/Form1.cs
+++ Testing Repo/Testing Repo/Form1.cs
@@ -16,5 +16,10 @@ namespace Testing_Repo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            return;
+        }
     }
 }
